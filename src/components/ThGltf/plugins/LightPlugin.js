eafdc825ad232cda742b3de7c8c42080d3bf254d
baix <baix@baix.com>
2020-12-12 14:00:40 +0800

    调整光强

--- src/components/ThGltf/plugins/LightPlugin.js
+++ src/components/ThGltf/plugins/LightPlugin.js
@@ -1,26 +1,26 @@
-import {DirectionalLight,Vector3,HemisphereLight,PointLight} from 'three'
+import { DirectionalLight, Vector3, HemisphereLight } from 'three'
 // 平行光不衰减
-const directionalLight = new DirectionalLight( 0xffffff, 1 ) //  添加了一个白色的平行光(颜色,光强)
+const directionalLight = new DirectionalLight(0xffffff, 0.6) //  添加了一个白色的平行光(颜色,光强)
 directionalLight.position.set(0, 30, 30) //  设置光源位置
 directionalLight.lookAt(new Vector3())
 
 // 背后平行光
-const behindDirectionalLight = new DirectionalLight(0xffffff,1)
+const behindDirectionalLight = new DirectionalLight(0xffffff, 1)
 behindDirectionalLight.position.set(0, 30, -30)
 behindDirectionalLight.lookAt(new Vector3())
 
 // 左侧平行光
-const leftDirectionalLight = new DirectionalLight(0xffffff,1)
+const leftDirectionalLight = new DirectionalLight(0xffffff, 1)
 leftDirectionalLight.position.set(30, 30, 0)
 leftDirectionalLight.lookAt(new Vector3())
 
 // 右侧平行光
-const rightDirectionalLight = new DirectionalLight(0xffffff,1)
+const rightDirectionalLight = new DirectionalLight(0xffffff, 1)
 rightDirectionalLight.position.set(-30, 30, 0)
 rightDirectionalLight.lookAt(new Vector3())
 
 // 半球光
-const hemisphereLight = new HemisphereLight( 0xffffff, 0xffffff, 1 )
-hemisphereLight.position.set( 0, 30, 0 )
+const hemisphereLight = new HemisphereLight(0xffffff, 0xffffff, 0.8)
+hemisphereLight.position.set(0, 30, 0)
 
-export { directionalLight, behindDirectionalLight,leftDirectionalLight,rightDirectionalLight, hemisphereLight }
+export { directionalLight, behindDirectionalLight, leftDirectionalLight, rightDirectionalLight, hemisphereLight }
--- src/components/ThGltf/plugins/ScenePlugin.js
+++ src/components/ThGltf/plugins/ScenePlugin.js
@@ -1,38 +1,38 @@
-import {Scene,Color,AmbientLight,Fog,Mesh,PlaneBufferGeometry,
-  MeshPhongMaterial,GridHelper,HemisphereLightHelper,
-  DirectionalLightHelper,MeshLambertMaterial} from 'three'
-import {directionalLight,behindDirectionalLight,
-  leftDirectionalLight,rightDirectionalLight,hemisphereLight} from './LightPlugin'
+/* eslint-disable no-unused-vars */
+import { Scene, Color, AmbientLight, Fog, Mesh, PlaneBufferGeometry,
+  MeshPhongMaterial, GridHelper, HemisphereLightHelper,
+  DirectionalLightHelper, MeshLambertMaterial } from 'three'
+import { directionalLight, behindDirectionalLight,
+  leftDirectionalLight, rightDirectionalLight, hemisphereLight } from './LightPlugin'
 
-export default function createScene(backgroundColor,AmbientLightColor,mode){
+export default function createScene(backgroundColor, AmbientLightColor, mode) {
   const scene = new Scene()
 
   // 设置背景颜色
   scene.background = new Color(backgroundColor)
-  scene.fog = new Fog( scene.background, 1, 1000 )
+  scene.fog = new Fog(scene.background, 1, 1000)
 
   // 地面网格
-  const grid = new GridHelper( 2000, 40, 0x000000, 0x000000 )
+  const grid = new GridHelper(2000, 40, 0x000000, 0x000000)
   grid.material.opacity = 0.2
   grid.material.transparent = true
-  scene.add( grid )
+  scene.add(grid)
 
   // scene.add(new AmbientLight(AmbientLightColor)) // 添加一个全局环境光
 
   scene.add(directionalLight) // 添加平行光
-  const dirLightHelper = new DirectionalLightHelper( directionalLight, 10 )
+  const dirLightHelper = new DirectionalLightHelper(directionalLight, 10)
 
   // scene.add(behindDirectionalLight) // 添加背部光源
   // scene.add(leftDirectionalLight) // 添加左侧光源
   // scene.add(rightDirectionalLight) // 添加右侧光源
 
   scene.add(hemisphereLight) // 添加半球光
-  const hemiLightHelper = new HemisphereLightHelper( hemisphereLight, 10 )
-  
+  const hemiLightHelper = new HemisphereLightHelper(hemisphereLight, 10)
 
-  if(mode==='dev'){
-    scene.add( dirLightHelper )
-    scene.add( hemiLightHelper )
+  if (mode === 'dev') {
+    scene.add(dirLightHelper)
+    scene.add(hemiLightHelper)
   }
 
   return scene
